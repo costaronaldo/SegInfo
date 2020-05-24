96b942159edbb02e184d97526b956628542f27c5
Ronaldo Costa <ronaldocosta@oceanica.ufrj.br>
2020-05-24 03:28:20 -0300

    correção da saída da mensagem em utf-8

--- aula-cripto/servidor.py
+++ aula-cripto/servidor.py
@@ -1,16 +1,22 @@
 import socket
-HOST = input(str("Insira o endereco IP: "))  # Endereco IP do Servidor
-PORT = 5000                                  # Porta que o Servidor esta
+
+HOST = input(str("Insira o endereço IP: "))  # Endereço IP do servidor
+PORT = 5000                                  # Porta que o servidor está
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
+
 while True:
+    print('\nAguardando conexão com o cliente...\n')
+    
     con, cliente = tcp.accept()
-    print ('Conectado por', cliente)
+    print ('Conectado por', cliente, '\n')
+    
     while True:
-        msg = con.recv(1024)
+        msg = con.recv(1024).decode("utf-8", "ignore")
         if not msg: break
-        print (cliente, msg)
-    print ('Finalizando conexao do cliente', cliente)
+        print ('Mensagem do cliente', cliente, '->', msg)
+    print ('Finalizando conexão do cliente', cliente)
     con.close()
