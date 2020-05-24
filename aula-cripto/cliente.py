173416e90f91423718ae345581feef3929be9575
Ronaldo Costa <ronaldocosta@oceanica.ufrj.br>
2020-05-24 03:26:44 -0300

    correção pontual no input

--- aula-cripto/cliente.py
+++ aula-cripto/cliente.py
@@ -1,11 +1,16 @@
 import socket
-HOST = input(str("Insira o endereco IP do servidor: "))  # Endereco IP do Servidor
-PORT = 5000                                              # Porta que o Servidor esta
+
+HOST = input(str("Insira o endereço IP do servidor: "))  # Endereço IP do servidor
+PORT = 5000                                              # Porta que o servidor está
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
-print ('Para sair use CTRL+X\n')
+
+print ('\nPara sair use CTRL+X e Enter\n')
+
 mensagem = input("Digite sua mensagem: ")
+
 while mensagem != "\x18":
     tcp.sendall(bytes(mensagem, 'utf-8'))
     mensagem = input("Digite sua mensagem: ")
